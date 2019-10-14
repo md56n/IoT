51ac40cd777c439ec4aa7f33fd9578e18a5e0677
md56n <47092603+md56n@users.noreply.github.com>
2019-10-13 22:14:11 -0500

    Add files via upload

---
+++ ICP 1/Code/ICP1.ino
@@ -0,0 +1,32 @@
+int switchState = 0;
+void setup() {
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(2, INPUT);
+}
+
+void loop(){
+  switchState = digitalRead(2);
+  if(switchState == LOW){ // the button is not pressed
+  digitalWrite(3, HIGH); //red LED
+  digitalWrite(4, LOW); //yellow LED
+  digitalWrite(5, LOW); //green LED
+  }
+  else{
+  digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+
+  delay(500); // wait for a half second
+  digitalWrite(3, LOW);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, LOW);
+
+  delay(500); // wait for a half second
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, HIGH);
+  delay(500);
+  }
+}
